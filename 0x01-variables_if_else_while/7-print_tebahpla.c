7979b77490e68e32c3bc914ebd79cee169811373
Gianluca <gianlucadorelo@gmail.com>
2020-09-17 17:10:43 +0000

    Added new task

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-  char c;
-  c = 'z';
-  while (c >= 'a')
-    {
-      putchar (c);
-      c = c - 1;
-    }
-  putchar ('\n');
-  return (0);
+char c;
+c = 'z';
+while (c >= 'a')
+{
+putchar (c);
+c = c - 1;
+}
+putchar ('\n');
+return (0);
 }
