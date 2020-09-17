cc9d6c06bbe1fcfd21e7b5172ecbfdc04b873c99
Gianluca <gianlucadorelo@gmail.com>
2020-09-17 16:08:13 +0000

    Removed spaces

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
-
 /**
  * main - entry point
  * Return: always 0 (success)
  */
 int main(void)
 {
-  char c;
-
-  c = 'a';
-
-  while (c <= 'z')
-    {
-      putchar (c);
-      c = c + 1;
-    }
-  putchar ('\n');
-
-  return (0);
+char c;
+c = 'a';
+while (c <= 'z')
+{
+putchar (c);
+c = c + 1;
+}
+putchar ('\n');
+return (0);
 }
