cba8cf94e7a8292b5aca3a0e1d73675ce3c2e543
Gianluca <gianlucadorelo@gmail.com>
2020-09-22 00:43:00 +0000

    Added new tasks

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+void times_table(void);
+
+{
+	int x;
+	int z:
+	x = 0;
+	z = 0;
+	for (x >= 9)
+	{
+		_putchar(z * x);
+		_putchar(',');
+		_putchar(' ');
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -7,3 +7,58 @@ int _putchar(char c);
  * print alphabet in lowercase
  */
 void print_alphabet(void);
+
+/**
+ * print alphabet ten times
+ */
+void print_alphabet_x10(void);
+
+/**
+ * check for lowercase character
+ */
+int _islower(int c);
+
+/**
+ * check for alphabetic character
+ */
+int _isalpha(int c);
+
+/**
+ * print the sign of a number
+ */
+int print_sign(int n);
+
+/**
+ * computer absolute value of integer
+ */
+int _abs(int);
+
+/**
+ * print last digit of a number
+ */
+int print_last_digit(int);
+
+/**
+ *prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ */
+void jack_bauer(void);
+
+/**
+ * print 9 times table, starting with 0
+ */
+void times_table(void);
+
+/**
+ * add two integers and return the result
+ */
+int add(int, int);
+
+/**
+ * print all natural numbers from n to 98, followed by a new line.
+ */
+void print_to_98(int n);
+
+/**
+ * print the n times table, starting with 0
+ */
+void print_times_table(int n);
