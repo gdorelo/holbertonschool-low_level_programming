782c489b7a3c6271fb5677d12be36737f0420b16
Gianluca <gianlucadorelo@gmail.com>
2020-09-28 21:32:29 +0000

    Added new task

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints every other character from a string
+ * @str: character
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i) != '\0'; i = i + 2)
+	{
+		_putchar (*(str + i));
+	}
+	_putchar ('\n');
+}
