25ce838dd888a5947c4bbc240265817202f58978
Gianluca <gianlucadorelo@gmail.com>
2020-09-29 12:40:04 +0000

    Added new task

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,15 +4,15 @@
  * _strcpy - copies string pointed to by src
  * @dest: character pointer
  * @src: character source
- * Return: the pointer to dest
+ * Return: the pointer to des
  */
 char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; *(src + i) != '\0'; i++)
+	for (i = 0; src [i] != '\0'; i++)
 	{
-		*(dest + i) = *(src + i);
+		dest [i] = src [i];
 	}
 
 	return (dest);
