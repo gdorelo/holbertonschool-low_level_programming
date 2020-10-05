75fe29225e7b5b0055317f1608f6d756a5d6e692
Gianluca <gianlucadorelo@gmail.com>
2020-10-05 20:07:17 +0000

    Added new task

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,9 +8,9 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i;
-	int sum1 = 0;
-	int sum2 = 0;
+	int i, sum1, sum2;
+
+	sum1 = sum2 = 0;
 
 	for (i = 0; i < (size * size); i += size + 1)
 		sum1 += a[i];
