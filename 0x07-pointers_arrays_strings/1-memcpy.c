23a9cec1f1521e64265c417e364d1d289fd59c67
Gianluca <gianlucadorelo@gmail.com>
2020-10-05 15:17:04 +0000

    Added new task

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,14 +8,9 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i = 0;
-	char *src_char = (char *)src;
-	char *dest_char = (char *)dest;
+	unsigned int i;
 
-	while (i < n)
-	{
-		dest_char[i] = src_char[i];
-		i++;
-	}
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 	return (dest);
 }
