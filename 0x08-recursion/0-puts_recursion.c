7042aa56ebc5979b4eda9fe92bd0f28caa3b10d6
Gianluca <gianlucadorelo@gmail.com>
2020-10-07 15:41:11 +0000

    Added new task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *_puts_recursion - prints string followed by new line
+ *@s: string
+ *
+ *Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
