54ddf0665c696d2932d40c3647a723c1d2777cb4
Gianluca <gianlucadorelo@gmail.com>
2020-10-15 00:48:22 +0000

    Added task(s)

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * *argstostr - concatenate all the arguments in the command line
+ * @ac: number of arguments
+ * @av: arguments
+ * Return: new string with a new line between arguments
+ */
+char *argstostr(int ac, char **av)
+{
+
+	if (ac == 0 || av == NULL)
+		return (0);
+
+	return (0);
+}
