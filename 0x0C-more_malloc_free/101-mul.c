466e459ef07745a72414d0fb527d7ba3a29910af
Gianluca <gianlucadorelo@gmail.com>
2020-10-15 19:30:53 +0000

    Added new task

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,25 +11,25 @@
 
 int main(int argc, char *argv[])
 {
-        long int num1, num2, mul;
-        if (argc != 3)
-        {
-                printf("Error\n");
-                exit(98);
-        }
-        num1 = atoi(argv[1]);
-        if (num1 == 0 && *argv[1] != 48)
-        {
-                printf("Error\n");
-                exit(98);
-        }
-        num2 = atoi(argv[2]);
-        if (num2 == 0 && *argv[2] != 48)
-        {
-                printf("Error\n");
-                exit(98);
-        }
-        mul = num1 * num2;
-        printf("%ld\n", mul);
-        return (0);
+long int num1, num2, mul;
+if (argc != 3)
+{
+printf("Error\n");
+exit(98);
+}
+num1 = atoi(argv[1]);
+if (num1 == 0 && *argv[1] != 48)
+{
+printf("Error\n");
+exit(98);
+}
+num2 = atoi(argv[2]);
+if (num2 == 0 && *argv[2] != 48)
+{
+printf("Error\n");
+exit(98);
+}
+mul = num1 * num2;
+printf("%ld\n", mul);
+return (0);
 }
