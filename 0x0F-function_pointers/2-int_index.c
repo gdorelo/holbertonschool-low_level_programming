261268133ce0a922b20bdde6409d35c145e877c2
Gianluca <gianlucadorelo@gmail.com>
2020-10-21 16:47:43 +0000

    Added new task

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,6 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if ((*cmp) == '\0' || array == '\0' || size <= 0)
 		return (-1);
+
 	for ( ; c < size; c++)
 		if ((*cmp)(array[c]))
 			return (c);
