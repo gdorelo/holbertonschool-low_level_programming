4e286fe9ff131187a0ee20822c2fb326526d0d80
Gianluca <gianlucadorelo@gmail.com>
2020-10-21 20:22:18 +0000

    Added new task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: arg counter
+ * @argv: arguments
+ * Return: 0 Success
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	char *p = (char*)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	while (i < (atoi(argv[1])))
+	{
+		printf("%02hhx ", *p);
+		i++;
+		p++;
+	}
+	printf("\n");
+	return (0);
+}
