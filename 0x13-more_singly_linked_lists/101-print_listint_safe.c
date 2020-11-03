be834266d879c80147b766b38c6a2aa9736debdd
Gianluca <gianlucadorelo@gmail.com>
2020-11-03 18:52:09 +0000

    Added new task

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -13,7 +13,7 @@ size_t print_listint_safe(const listint_t *head)
 
 	if (!head)
 		return (98);
-	while (slow_p && fast_p && fast_p->next)
+	while (fast_p)
 	{
 		printf("[%p] %d\n", (void *)head, head->n);
 		head = head->next;
