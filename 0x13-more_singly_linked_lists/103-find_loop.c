d0302d38456c8ba73d6969306364a8a832e10900
Gianluca Dorelo <66336015+gdorelo@users.noreply.github.com>
2020-11-04 15:05:53 -0300

    Create 103-find_loop.c

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * find_listint_loop - find the loop in a linked list
+ * @head: head of linked list
+ * Description: Not allowed to use malloc, free or arrays.
+ * Can only declare a max of 2 variables.
+ * Return: Address of node where loop starts, or NULL if no loop found.
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+
+}
