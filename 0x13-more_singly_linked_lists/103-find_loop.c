6f96c99a0f4ed64b0ea93418f3a78213f2ecf370
Gianluca Dorelo <66336015+gdorelo@users.noreply.github.com>
2020-11-04 15:10:01 -0300

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -6,9 +6,37 @@
  * Can only declare a max of 2 variables.
  * Return: Address of node where loop starts, or NULL if no loop found.
  */
-
 listint_t *find_listint_loop(listint_t *head)
 {
-if (!head)
+	listint_t *fast_ptr, *slow_ptr;
+	bool is_loop;
+
+	if (head == NULL)
 		return (NULL);
+	fast_ptr = head;
+	slow_ptr = head;
+	is_loop = false;
+
+	while (fast_ptr && slow_ptr && fast_ptr->next)
+	{
+		fast_ptr = fast_ptr->next->next;
+		slow_ptr = slow_ptr->next;
+
+		if (slow_ptr == fast_ptr)
+		{
+			is_loop = true;
+			break;
+		}
+	}
+	if (is_loop)
+	{
+		fast_ptr = head->next;
+		slow_ptr = head;
+	}
+	while (fast_ptr && fast_ptr < slow_ptr && is_loop)
+	{
+		slow_ptr = slow_ptr->next;
+		fast_ptr = fast_ptr->next;
+	}
+	return (fast_ptr);
 }
