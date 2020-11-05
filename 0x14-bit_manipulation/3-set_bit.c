ca6dd16f4776d29d934eb0eb7a281123c0732a0f
Gianluca Dorelo <66336015+gdorelo@users.noreply.github.com>
2020-11-05 15:36:08 -0300

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,12 +7,13 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-        unsigned long int i = 1;
-        if (n == NULL)
-                return (-1);
-        if (index > sizeof(unsigned long int) * 8)
-        return (-1);
-        i <<= index;
-        *n = *n | i;
-        return (1);
+	unsigned long int i = 1;
+
+	if (n == NULL)
+		return (-1);
+	if (index > sizeof(unsigned long int) * 8)
+	return (-1);
+	i <<= index;
+	*n = *n | i;
+	return (1);
 }
