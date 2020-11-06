a4beb3f121ce300fc50fb842800549f65f77fd46
Gianluca <gianlucadorelo@gmail.com>
2020-11-06 18:01:43 +0000

    Added new task

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,82 +1,31 @@
 #include "lists.h"
 /**
- *loopexist - check if loop exist whitin list
- *@head: head of list
- *@pl: pointer to loop value
- *@pnumber: pointer to number
- *Return: pointer to fast loop
- */
-const listint_t *loopexist(const listint_t *head, int *pl, int *pnumber);
-/**
  *print_listint_safe - prints all the elements of a listint
  *@head: head of list
  *Return: number of elements
  */
 size_t print_listint_safe(const listint_t *head)
 {
+	const listint_t *slow_ptr, *fast_ptr, *free_ptr;
+	size_t size;
 
-	int loop = 0, *pl = &loop;
-	int number = 0, *pnumber = &number, node = 0;
-	const listint_t *sp = head, *fp = head, *hl = NULL;
+	size = 0;
 
-	if (head == NULL)
-	{
-		return (0);
+	if (!head)
 		exit(98);
-	}
-	fp = loopexist(head, pl, pnumber);
-	if (loop == 1)
-	{
-		sp = head;
-		while (fp != sp)
-		{
-			sp = sp->next;
-			fp = fp->next;
-			number++;
-		}
-		sp = head;
-		node = number;
-		while (node > 0)
-		{
-			printf("[%p] %d\n", (void *)sp, sp->n);
-			sp = sp->next;
-			node--;
-			hl = sp;
-		}
-		printf("-> [%p] %d\n", (void *)hl, hl->n);
-	}
-	else
-	{
-		while (head)
-		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
-		}
-	}
-	return (number);
-}
-/**
- *loopexist - check if loop exist whitin list
- *@head: head of list
- *@pl: pointer to loop value
- *@pnumber: pointer to number
- *Return: pointer to fast loop
- */
-const listint_t *loopexist(const listint_t *head, int *pl, int *pnumber)
-{
-	const listint_t *fp = head, *sp = head;
+	fast_ptr = head->next;
+	slow_ptr = head;
 
-	while (fp)
+	while (fast_ptr && fast_ptr < slow_ptr)
 	{
-		sp = sp->next;
-		fp = fp->next->next;
-		(*pnumber)++;
-		if (sp == fp)
-		{
-			*pl = 1;
-			return (fp);
-		}
+		free_ptr = slow_ptr;
+		fast_ptr = fast_ptr->next;
+		slow_ptr = slow_ptr->next;
+		size += 1;
+
+		printf("[%p] %d\n", (void *)free_ptr, free_ptr->n);
 	}
-	*pl = 0;
-	return (NULL);
+	size += 1;
+	printf("[%p] %d\n", (void *)slow_ptr, slow_ptr->n);
+	return (size);
 }
