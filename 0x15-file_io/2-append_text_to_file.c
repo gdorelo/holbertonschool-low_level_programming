24ea9767ba4d5f17b035329c9b658a9a6706a20b
Gianluca Dorelo <66336015+gdorelo@users.noreply.github.com>
2020-11-09 19:10:55 -0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,17 +1,16 @@
 #include "holberton.h"
 /**
-* append_text_to_file - appens text at the end of a file
-* @filename: the file to append to
-* @text_content: the text that will be appended to the file
-* Return: 1 (Success), -1 (Failure)
-*/
+ * append_text_to_file - function that appends text ot a file
+ * @filename: name of the file to read
+ * @text_content: content of the text
+ * Return: 1 on success, -1 on failure
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, i = 0, len;
 
 	if (filename == NULL)
 		return (-1);
-	
 	if (text_content == NULL)
 		text_content = "";
 	fd = open(filename, O_RDWR | O_APPEND);
@@ -20,7 +19,6 @@ int append_text_to_file(const char *filename, char *text_content)
 		close(fd);
 		return (-1);
 	}
-	
 	while (text_content[i])
 		i++;
 	len = write(fd, text_content, i);
