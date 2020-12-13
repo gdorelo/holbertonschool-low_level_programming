b9185939e01bc9d163d421effbc514e2f1922997
Gianluca <gianlucadorelo@gmail.com>
2020-12-13 16:11:03 +0000

    added more tasks

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,8 +2,8 @@
 /**
  * add_dnodeint_end - add node to the end of a list
  * @head: double pointer to the start of the list.
- * @n: integer to assign to the new_node node
- * Return: pointer to new_node node
+ * @n: integer to assign to the new_node 
+ * Return: pointer to new_node 
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
@@ -29,28 +29,5 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		}
 		aux = aux->next;
 	}
-	return (new_node);
-}
-/**
- * add_dnodeint - add node to the start of a list
- * @head: double pointer to the start of the list.
- * @n: integer to assign to the new_node node
- * Return: pointer to new_node node
- */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
-{
-	dlistint_t *new_node;
-
-	if (!head)
-		return (0);
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = *head;
-	new_node->prev = NULL;
-	if (*head != NULL)
-		(*head)->prev = new_node;
-	*head = new_node;
 	return (new_node);
 }
