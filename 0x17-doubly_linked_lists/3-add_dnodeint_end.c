4d80b26e769234a3f2f56fcc95e438556ae86418
Gianluca <gianlucadorelo@gmail.com>
2020-12-13 16:20:20 +0000

    betty

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -32,7 +32,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	return (new_node);
 }
 /**
- * add_dnodeint - add node to the start of a list
+ * addd_dnodeint - add node to the start of a list
  * @head: double pointer to the start of the list.
  * @n: integer to assign to the new_node
  * Return: pointer to new_node
