ec4ff80312a6a379c23f2445af49e4ae632383f8
Gianluca <gianlucadorelo@gmail.com>
2020-12-13 16:18:21 +0000

    Edge cases

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (!head)
 		return (0);
 	if (*head == NULL)
-		return (add_dnodeint(head, n));
+		return (addd_dnodeint(head, n));
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -29,5 +29,28 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		}
 		aux = aux->next;
 	}
+	return (new_node);
+}
+/**
+ * add_dnodeint - add node to the start of a list
+ * @head: double pointer to the start of the list.
+ * @n: integer to assign to the new_node 
+ * Return: pointer to new_node 
+ */
+dlistint_t *addd_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	if (!head)
+		return (0);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = new_node;
+	*head = new_node;
 	return (new_node);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -28,5 +28,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-dlistint_t *adddnodeint(dlistint_t **head, const int n);
+dlistint_t *addd_dnodeint(dlistint_t **head, const int n);
 #endif
